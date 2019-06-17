bc7accd6252c63661a2c69ce9e13d40fe2e876e4
pavanraparthi <pavan.raparthi@gmail.com>
2019-06-17 14:21:32 +0530

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,22 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Bubble Sort</h1>`;
+
+var arr = [2,15,6,7,3,4,0,15,8,9]
+
+function sort_bubble(arr){
+  var isSwapped = false;
+  do{
+    isSwapped = false;
+    arr.forEach((num, i) => {
+      if(num > arr[i+1]){
+        var temp = arr[i+1];
+        arr[i+1] = num;
+        arr[i] = temp;
+        isSwapped = true;
+      }
+    })
+  }while(isSwapped);
+  return arr;
+}
+console.log(sort_bubble(arr));
